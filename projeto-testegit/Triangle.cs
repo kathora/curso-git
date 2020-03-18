91835212d9ee90969aa33a3a24f9b2998336de76
kathora <kathorahora@gmail.com>
2020-03-18 10:37:45 +0000

    Novo Delegate criando funcao

--- projeto-testegit/Triangle.cs
+++ projeto-testegit/Triangle.cs
@@ -10,5 +10,12 @@ namespace projeto_testegit
         public double B;
         public double C; 
 
+        public double Area() //esta função não leva parametros pk está dentro da class
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+
     }
 }
